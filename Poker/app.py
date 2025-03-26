c907c5d9e8b3297c594e83859d4cc3e3fa16426c
Rafael Pires Gonçalves <127984148+Rafael1572008@users.noreply.github.com>
2025-03-25 21:53:40 -0300

    MELHORIA

--- Poker/app.py
+++ Poker/app.py
@@ -8,26 +8,36 @@ def hello_world():
 
     return redirect(url_for('requisicao'))
     
-@app.route("/hello", methods = ['GET'])
+@app.route("/hello")
 def requisicao():
-    name = "pikachu"
+    name_get = "mewtwo"
+
+    # Caos rode em lugar que tenha prox (IFSP)
+    """proxies = {
+        "http": "proxy.spo.ifsp.edu.br:3128",
+        "https": "http://proxy.spo.ifsp.edu.br:3128"
+    } 
+        
+        response = requests.get(
+        "https://pokeapi.co/api/v2/pokemon/bulbasaur",
+        proxies=proxies,
+        verify=False
+    ) """
+
+    response = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{name_get}").text)
+
+    img_poker = response['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+    name_poker = response['forms'][0]['name']
+    id_poker = response['id']
+
+
+    info_poker = {"name_poker": name_poker,
+                  "id_poker": id_poker,
+                  "img_poker": img_poker,
+                  }
     
-    proxies = {
-    "http": "proxy.spo.ifsp.edu.br:3128",
-    "https": "http://proxy.spo.ifsp.edu.br:3128"
-}
-    
-    response = requests.get(
-    "https://pokeapi.co/api/v2/pokemon/bulbasaur",
-    proxies=proxies,
-    verify=False
-)
-    
-    response = json.loads(response.text)['sprites']['front_default']
-    
-    return render_template('final.html', response = response)
-    
-    
+    return render_template('final.html', info_poker = info_poker)
+
     
 
 if __name__ == '__main__':
